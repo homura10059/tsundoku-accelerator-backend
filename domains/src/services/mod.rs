f143f98dbd446080225a72c5a4e3e8fe011c3e31
homura <preasper0+github@gmail.com>
2022-09-24 13:22:10 +0900

    use db data

--- domains/src/services/mod.rs
+++ domains/src/services/mod.rs
@@ -1,30 +1,21 @@
 use crate::infrastructures::db::prisma::PrismaClient;
-use crate::infrastructures::db::wish_list;
 use crate::infrastructures::{db, scraper};
 use anyhow::Result;
 use futures::future;
 
-pub async fn update_wish_list(client: &PrismaClient, id: &str) -> Result<()> {
-    let snapshot = scraper::get_wish_list_snapshot(id)?;
-    let result = wish_list::upsert_wish_list(client, &snapshot).await?;
+pub async fn update_wish_list(client: &PrismaClient, id: String) -> Result<()> {
+    let snapshot = scraper::get_wish_list_snapshot(id.as_str())?;
+    let result = db::wish_list::upsert_wish_list(client, &snapshot).await?;
     Ok(result)
 }
 
 pub async fn update_all_wish_list() -> Result<()> {
-    let id_list = vec![
-        "2BDAPI9RQ09E9",
-        "2VJWMFFVPSTEP",
-        "1CU482XA9HO5F",
-        "3MLOGSF4M4JP",
-        "3E1B11VRAD7UZ",
-        "1KERB2C8N5NGY",
-        "211LVEVFIA3JT",
-    ];
     let client = db::get_client().await;
+    let lists = db::wish_list::select_all_wish_list(&client).await?;
 
-    let results = id_list
+    let results = lists
         .into_iter()
-        .map(|id| update_wish_list(&client, id))
+        .map(|list| update_wish_list(&client, list.id))
         .collect::<Vec<_>>();
     future::join_all(results).await;
     Ok(())
@@ -40,7 +31,9 @@ mod tests {
         dotenv::dotenv().ok();
         let client = db::get_client().await;
 
-        let actual = update_wish_list(&client, "2BDAPI9RQ09E9").await.unwrap();
+        let actual = update_wish_list(&client, String::from("2BDAPI9RQ09E9"))
+            .await
+            .unwrap();
         assert_eq!(actual, ());
     }
 

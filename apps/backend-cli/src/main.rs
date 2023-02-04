8726f4fdbfd66d6dd37e6ff476b0c5091a864dd2
homura <preasper0+github@gmail.com>
2023-02-04 21:13:27 +0900

    add error handling

--- apps/backend-cli/src/main.rs
+++ apps/backend-cli/src/main.rs
@@ -2,6 +2,7 @@ use clap::{Parser, Subcommand};
 use domains::notifications;
 use domains::wish_lists;
 use dotenv;
+use std::result;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -24,7 +25,20 @@ async fn main() {
     let args = Args::parse();
 
     match args.command {
-        Commands::UpdateAllWishlist => wish_lists::update_all_wish_list().await.unwrap(),
+        Commands::UpdateAllWishlist => {
+            let result = wish_lists::update_all_wish_list().await;
+            match result {
+                Ok(_) => {
+                    // 成功してれば通知しない
+                }
+                Err(e) => {
+                    notifications::send_alert_message(e.to_string())
+                        .await
+                        .unwrap();
+                    ()
+                }
+            }
+        }
         Commands::SendNotification => {
             let data = wish_lists::services::select_all_wish_list_and_snapshot()
                 .await

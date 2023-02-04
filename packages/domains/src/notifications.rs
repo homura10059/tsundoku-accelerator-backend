2bcd03ea17b10cd402a1f6ae28d6c3d60d06ee5e
homura <preasper0+github@gmail.com>
2023-02-04 21:02:05 +0900

    use into

--- packages/domains/src/notifications.rs
+++ packages/domains/src/notifications.rs
@@ -42,6 +42,12 @@ enum EmbedColor {
     Grey = 9807270,
 }
 
+impl Into<String> for EmbedColor {
+    fn into(self) -> String {
+        (self as i32).to_string()
+    }
+}
+
 fn get_color(snapshot: &EBookSnapShotData) -> EmbedColor {
     let discount_rate = snapshot.discount_rate.unwrap_or(0.0);
     let points_rate = snapshot.points_rate;
@@ -119,81 +125,44 @@ impl EmbedItem {
     }
 }
 
-fn convert_from(data: &WishListData) -> Result<Vec<Message>> {
-    let config = envy::prefixed("DISCORD_").from_env::<Config>()?;
+fn convert_from(data: &WishListData) -> Option<Vec<Message>> {
+    let notification = SaleNotification::new(data)?;
 
-    let message_title = format!("{} のセール情報", data.title);
+    let chunks = notification.embeds.chunks(10).collect::<Vec<_>>();
 
-    let ebook_in_wish_list = data
-        .ebook_in_wish_list
-        .clone()
-        .ok_or(anyhow!("no ebooks"))?;
-    let ebooks = ebook_in_wish_list
-        .iter()
-        .filter_map(|x| x.ebook.clone())
-        .collect::<Vec<_>>();
-    let chunks = ebooks.chunks(10).collect::<Vec<_>>();
     let result = chunks
         .iter()
-        .map(|ebooks| {
+        .map(|items| {
             let mut message = Message::new();
-            message.username(config.bot_name.as_ref());
-            message.avatar_url(config.avatar_url.as_ref());
-            message.content(message_title.as_ref());
+            message.username(notification.from_user_name.as_str());
+            message.avatar_url(notification.from_avatar_url.as_str());
+            message.content(notification.content.as_str());
 
-            for ebook in *ebooks {
-                if ebook.snapshots.is_none() {
-                    continue;
-                }
-                let snapshots = ebook.snapshots.clone().unwrap();
-                let latest_snapshot = snapshots.get(0);
-                if latest_snapshot.is_none() {
-                    continue;
-                }
-                let snap = latest_snapshot.unwrap();
-                let color = get_color(snap);
-                if color == EmbedColor::Grey {
-                    continue;
-                }
+            for item in *items {
+                let color: String = item.color.into();
                 message.embed(move |embed| {
-                    let offset = FixedOffset::east_opt(9 * 3600).unwrap();
-                    let date = offset.timestamp_opt(snap.scraped_at, 0).unwrap();
-                    let color_num = color as i32;
-
                     embed
-                        .title(ebook.title.as_str())
-                        .url(ebook.url.as_ref())
-                        .color(color_num.to_string().as_ref())
-                        .field("金額", format!("¥{}", snap.price).as_ref(), true)
-                        .field(
-                            "値引き率",
-                            format!("{}%", snap.discount_rate.unwrap_or(0.0)).as_ref(),
-                            true,
-                        )
-                        .field(
-                            "ポイント還元率",
-                            format!("{}%", snap.points_rate).as_ref(),
-                            true,
-                        )
-                        .field(
-                            "更新日",
-                            date.format("%Y/%m/%d %H:%M:%S %Z").to_string().as_ref(),
-                            true,
-                        )
+                        .title(item.title.as_str())
+                        .url(item.url.as_ref())
+                        .color(color.as_str())
+                        .field("金額", item.price.as_str(), true)
+                        .field("値引き率", item.discount_rate.as_str(), true)
+                        .field("ポイント還元率", item.points_rate.as_str(), true)
+                        .field("更新日", item.update_datetime.as_str(), true)
                 });
             }
             message
         })
         .collect::<Vec<_>>();
 
-    Ok(result)
+    Some(result)
 }
 
 pub async fn notify(data: &WishListData) -> Result<bool> {
     let config = envy::prefixed("DISCORD_").from_env::<Config>()?;
 
-    let messages = convert_from(data)?;
-    let client: WebhookClient = WebhookClient::new(config.alert_chanel.as_ref());
+    let messages = convert_from(data).ok_or(anyhow!("can not create message"))?;
+    let client: WebhookClient = WebhookClient::new(config.sale_chanel.as_ref());
     for message in messages {
         client.send_message(&message).await.unwrap();
     }
